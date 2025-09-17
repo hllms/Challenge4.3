d33e31e5ee21c518fafb2a8893c6d1adad4261ae
Jason Hellums <jason.hellums@outlook.com>
2025-09-17 13:21:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Challenge4._3
+{
+    internal class Program
+    {
+        static void ReverseArrayInplace<T>(T[] array)
+        {
+            T tempHead = default(T);
+            T tempTail = default(T);
+            for (int i = 0; i <= array.Length / 2; i++)
+            {
+                tempHead = array[i];
+                tempTail = array[array.Length - 1 - i];
+
+                array[i] = tempTail;
+                array[array.Length - 1 - i] = tempHead;
+            }
+            string output = string.Join(", ", array);
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            string[] array = { "H", "a", "n", "n", "a", "h" };
+            ReverseArrayInplace(array);
+            Console.WriteLine("and return");
+            ReverseArrayInplace(array);
+        }
+    }
+}
+
